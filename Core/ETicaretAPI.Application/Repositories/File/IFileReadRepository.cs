f0c8730dc46e47b371fc5c2d83a269782b70d005
emirhan <emir0sahbaz@gmail.com>
2025-03-24 17:35:49 +0300

    Table Per Hierarchy Yaklaşımı İle Dosyaların Kaydı

---
+++ Core/ETicaretAPI.Application/Repositories/File/IFileReadRepository.cs
@@ -0,0 +1,8 @@
+﻿using F = ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IFileReadRepository : IReadRepsitory<F::File> //farklı bir yol tür belirtmek için
+    {
+    }
+}
