0cb24e276fed9c01436096808d080eeb35c224b1
emirhan <emir0sahbaz@gmail.com>
2025-06-08 14:12:20 +0300

    Rol Yönetim Sayfasının Oluşturulması

---
+++ Infrastructure/ETicaretAPI.Persistence/Services/RoleService.cs
@@ -0,0 +1,49 @@
+﻿using ETicaretAPI.Application.Abstractions.Services;
+using ETicaretAPI.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace ETicaretAPI.Persistence.Services
+{
+    public class RoleService : IRoleService
+    {
+        readonly RoleManager<AppRole> _roleManager;
+
+        public RoleService(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<bool> CreateRole(string name)
+        {
+            IdentityResult result = await _roleManager.CreateAsync(new() { Id = Guid.NewGuid().ToString(), Name = name });
+            return result.Succeeded;
+        }
+
+        public async Task<bool> DeleteRole(string id)
+        {
+            AppRole appRole = await _roleManager.FindByIdAsync(id);
+            IdentityResult result = await _roleManager.DeleteAsync(appRole);
+            return result.Succeeded;
+        }
+
+        public (object, int) GetAllRoles(int page, int size)
+        {
+            var data = _roleManager.Roles.Skip(page * size).Take(size).Select(r => new { r.Id, r.Name });
+            return (data, _roleManager.Roles.Count());
+        }
+
+        public async Task<(string id, string name)> GetRoleById(string id)
+        {
+            string role = await _roleManager.GetRoleIdAsync(new() { Id = id });
+            return (id, role);
+        }
+
+        public async Task<bool> UpdateRole(string id, string name)
+        {
+            AppRole role = await _roleManager.FindByIdAsync(id);
+            role.Name = name;
+            IdentityResult reslut = await _roleManager.UpdateAsync(role);
+            return reslut.Succeeded;
+        }
+    }
+}
